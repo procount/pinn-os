67d1b3cdc073ae34791c73207ab7227892fd82f4
procount <kevin.procount@googlemail.com>
2018-12-06 13:11:37 +0000

    Original setup scripts

---
+++ partitions/TlxRpi/partition_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -ex
+
+if [ -z "$part1" ] || [ -z "$part3" ]; then
+  printf "Error: missing environment variable part1 or part3\n" 1>&2
+  exit 1
+fi
+
+mkdir -p /tmp/1
+mount "$part1" /tmp/1
+
+sed /tmp/1/cmdline.txt -i -e "s|root=/dev/[^ ]*|root=${part3}|"
+
+umount /tmp/1
---
+++ partitions/recalbox/partition_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -ex
+
+if [ -z "$part1" ] || [ -z "$part2" ] || [ -z "$part3" ]; then
+  printf "Error: missing environment variable part1 or part2 or part3\n" 1>&2
+  exit 1
+fi
+
+mkdir -p /tmp/1 /tmp/2
+
+mount "$part1" /tmp/1
+mount "$part2" /tmp/2
+
+sed /tmp/1/cmdline.txt -i -e "s|root=/dev/[^ ]*|root=${part2}|"
+
+umount /tmp/1
+umount /tmp/2
